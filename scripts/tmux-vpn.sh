c7c9b0b2f27ec0995d4057d1e40b01140ef1edc2
Wes van der Vleuten <me@wes.cloud>
2020-12-11 02:02:47 +0100

    Added more vpn tunnels to tmux line

--- scripts/tmux-vpn.sh
+++ scripts/tmux-vpn.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
-ip a s tun0 | grep -Po 'inet \K[\d.]+' || echo '!vpn'
+TUN0=`ip a s tun0 | grep -Po 'inet \K[\d.]+'`;
+TUN1=`ip a s tun1 | grep -Po 'inet \K[\d.]+'`;
+TUN2=`ip a s tun2 | grep -Po 'inet \K[\d.]+'`;
+
+if [ -z "$TUN0" ] && [ -z "$TUN1" ] && [ -z "$TUN2" ]
+then
+	echo '!vpn';
+	exit;
+fi
+
+OUTPUT="";
+
+if [ ! -z "$TUN0" ]
+then
+	OUTPUT="$OUTPUT t0:$TUN0";
+fi
+if [ ! -z "$TUN1" ]
+then
+	OUTPUT="$OUTPUT t1:$TUN1";
+fi
+if [ ! -z "$TUN2" ]
+then
+	OUTPUT="$OUTPUT t2:$TUN2";
+fi
+
+echo $OUTPUT;
