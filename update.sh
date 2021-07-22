c9fb0abc59f92ebfeaeec5d0a364ce0a0589f3bd
Wes van der Vleuten <16665772+WesVleuten@users.noreply.github.com>
2021-07-23 00:05:29 +0200

    Use new nvim to install/update plugins

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O /opt/nvim.appimage
 chmod +x /opt/nvim.appimage
 
-PLUGINSTALL=1 nvim -c "PlugUpgrade" -c "PlugUpdate" -c "qa"
+PLUGINSTALL=1 /opt/nvim.appimage -c "PlugUpgrade" -c "PlugUpdate" -c "qa"
 
